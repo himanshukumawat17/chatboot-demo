79fe6fd515275a5929d82e8bb6c09d2d2538b5ec
chouhan <ravindra.chouhan@vidhema.com>
2025-10-06 11:27:31 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 10026
 
 // Set up the OAuth constants
 const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY
